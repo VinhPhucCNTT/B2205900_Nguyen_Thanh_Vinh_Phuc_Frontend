fb4a60f3abd78d72990617acc071c187f5f53de1
Nguyen Thanh Vinh Phuc <phucb2205900@student.ctu.edu.vn>
2025-07-26 20:30:06 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page">
+        <h4>Thêm liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue"
+import contactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {},
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await contactService.create(data);
+                alert('Liên hệ được thêm thành công.');
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+}
+</script>
